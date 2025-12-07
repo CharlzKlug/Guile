cd559b917617f2c70dfb1d0cb3557fe2a93e821b
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-12-07 17:57:03 +0300

    5.2.4 Building the Example with Autoconf

---
+++ 05/02_Linking_Guile/04_Example_Autoconf/simple-guile.c
@@ -0,0 +1,19 @@
+/* simple-guile.c --- Start Guile from C. */
+/* nix-shell -p gcc pkg-config guile autoconf automake */
+/* gcc `pkg-config guile-3.0 --cflags --libs` main.c */
+/* or */
+/* $ autoreconf -vif */
+/* $ ./configure */
+/* $ make */
+#include <libguile.h>
+
+static void inner_main (void *closure, int argc, char **argv) {
+  /* preparation */
+  scm_shell (argc, argv);
+  /* after exit */
+}
+
+int main (int argc, char **argv) {
+  scm_boot_guile (argc, argv, inner_main, 0);
+  return 0; /* never reached, see inner_main */
+}
