20d6fd463451eb6a1f61bc2be00a917e6a93bcb7
Imil Ametov <charlz_klug@mail.ru>
2025-12-09 17:56:33 +0300

    5.5.2 Creating Foreign Objects

--- 05/05_Foreign_Types/main.c
+++ 05/05_Foreign_Types/main.c
@@ -29,6 +29,38 @@ void init_image_type (void) {
     scm_make_foreign_object_type (name, slots, finalizer);
 }
 
+SCM make_image (SCM name, SCM s_width, SCM s_height) {
+  struct image *image;
+  int width = scm_to_int (s_width);
+  int height = scm_to_int (s_height);
+
+  /* Allocate the `struct image'. Because we
+     use scm_gc_malloc, this memory block will
+     be automatically reclaimed when it becomes
+     inaccessible, and its member will be traced
+     by the garbage collector. */
+  image = (struct image *)
+    scm_gc_malloc (sizeof (struct image), "image");
+  image->width = width;
+  image->height = height;
+
+  /* Allocating the pixels with
+     scm_gc_malloc_pointerless means that the
+     pixels data is collectable by GC, but
+     that GC shouldn't spend time tracing its
+     contents for nested pointers because there
+     aren't any. */
+  image->pixels =
+    scm_gc_malloc_pointerless (width * height, "image pixels");
+
+  image->name = name;
+  image->update_func = SCM_BOOL_F;
+
+  /* Now wrap the struct image* in a new foreign
+     object, and return that object. */
+  return scm_make_foreign_object_1 (image_type, image);
+}
+
 int main() {
   scm_init_guile();
   init_image_type();
