48035070a56f7c95c0173caa3ff403839ed81fef
Imil Ametov <charlz_klug@mail.ru>
2025-12-24 12:08:49 +0300

    6.5 Snarfing Macros...

--- 06_API/05_Macros/main.c
+++ 06_API/05_Macros/main.c
@@ -1,9 +1,8 @@
 /* Compile in Fish: */
-/* eval gcc -o simple-guile simple-guile.c (pkg-config --cflags --libs
- * guile-3.0) */
+/*eval gcc -o simple-guile main.c (pkg-config --cflags --libs guile-3.0) */
 
 /* Использование guile-snarf: */
-/* eval guile-snarf (pkg-config --cflags guile-3.0) -o image-type.x main.c */
+/* eval guile-snarf -o image-type.x main.c (pkg-config --cflags guile-3.0) */
 #include <libguile.h>
 #include <stdlib.h>
 
@@ -16,8 +15,7 @@ SCM_DEFINE(my_add, "my-add", 2, 0, 0,
 }
 
 static void inner_main(void *data, int argc, char **argv) {
-  /* scm_c_define_gsubr("my-hostname", 0, 0, 0, my_hostname); */
-  /* #include "image-type.x" */
+#include "image-type.x"
   scm_shell(argc, argv);
 }
 
